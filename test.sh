d0d0aebe773e4d9403e2b98ccdf69af7ab9782cc
kitzian <72838760+kitzian@users.noreply.github.com>
2021-07-05 12:49:25 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 while :
 do
-	wget -O /dev/null http://speedtest.sjc01.softlayer.com/downloads/test10000.zip
+	wget -O /dev/null https://destiny.wtf/assets/others/KUSO%20GVKI%20-%20Cinnamon%20%28feat.%20Apfel%29%20%28splash%21%20Mag%20Premiere%29.mp4
 done
